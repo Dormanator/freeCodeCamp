2a82dd145901954b73cf0902e8ca8cdc466ec825
Ryan Dorman <RyanDormanThe1@gmail.com>
2017-12-06 19:39:03 -0600

    loc weather added

---
+++ projects/intermediateProjectSite/js/locWeather.js
@@ -0,0 +1,166 @@
+geoLocation();
+
+
+/****************************************************
+ * 
+ * 
+ *              WEATHER CONVERSION CLICK
+ * 
+ * 
+ ****************************************************/
+$("#convert").click(convert);
+
+function convert () {
+
+    // grab the format of teh temperature (C or F) and its value
+    let format = $("#temp").html().replace(/[^CF]/g,'');
+    let value = $("#temp").html().replace(/\D/g,'');
+
+    // if celcius convert to farenheit, else vice versa
+    if (format === 'C') {
+        value = Math.round((value * 9/5) + 32) + '&deg;F';
+    } else {
+        value = Math.round((value - 32) * 5/9) + '&deg;C';
+    }
+
+    // append converted temp to DOM
+    $("#temp").html(value);
+}
+
+/****************************************************
+ * 
+ * 
+ *              WEATHER ICON CONTROL
+ * 
+ * 
+ ****************************************************/
+
+    // function to display correct icon based on weather and time
+    function weatherIcon (rise, set, id) {
+
+        // finding current time to compare against sun rise and sun set
+        let currentTime = Math.round(Date.now()/1000);
+
+        // checking to see if its day or night based on sunrise and set, determine whether to show sun or moon when clear skies
+        let day = currentTime >= rise && currentTime < set;
+
+        // if else handling the icons based on type of weather https://openweathermap.org/weather-conditions
+
+        // severe weather
+        if (id >= 957) {
+            $("#image").html('<i class="fa fa-exclamation-triangle icon" aria-hidden="true"></i>');
+        // winds
+        } else if (id >= 952) {
+            $("#image").html('<i class="fa fa-flag icon" aria-hidden="true"></i>');
+        // clear during the day
+        } else if ( (id === 951 || id === 800) && day) {
+            $("#image").html('<i class="fa fa-sun-o icon" aria-hidden="true"></i>');
+        // clear at night
+        } else if (id === 951 || id === 800) {
+            $("#image").html('<i class="fa fa-moon-o" aria-hidden="true">')
+        // severe weather
+        } else if (id >= 900) {
+            $("#image").html('<i class="fa fa-exclamation-triangle icon" aria-hidden="true"></i>');
+        // clouds
+        } else if (id > 800) {
+            $("#image").html('<i class="fa fa-cloud icon" aria-hidden="true"></i>');
+        // atmospheric conditions
+        } else if (id > 700) { 
+            $("#image").html('<i class="fa fa-exclamation-triangle icon" aria-hidden="true"></i>');
+        // snow
+        } else if (id >= 600) {
+            $("#image").html('<i class="fa fa-snowflake-o icon" aria-hidden="true"></i>');
+        // rain
+        } else if (id >= 300) {
+            $("#image").html('<i class="fa fa-tint icon" aria-hidden="true"></i>');
+        // thunderstorms
+        } else {
+            $("#image").html('<i class="fa fa-bolt icon" aria-hidden="true"></i>');
+        }
+        
+    // console.log(day); // T or F for daytime
+    }
+
+/****************************************************
+ * 
+ * 
+ *              WEATHER API REQUEST
+ * 
+ * 
+ ****************************************************/
+
+function getWeather (lat, long) {
+    // save API address to call
+    const API = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long;
+    
+    // JSON API request with freeCodeCamp weather API and handleWeather as its callback function
+    $.getJSON(API).done(handleWeather).fail(noWeather);
+}
+
+function handleWeather (response) {
+    
+    // weather object to store values of interest from API
+    let weather = {
+    city: response.name,
+    id: response.weather[0].id,
+    desc: response.weather[0].description,
+    farenheit: Math.round((response.main.temp * 9/5) + 32) + '&deg;F',
+    sunrise: response.sys.sunrise,
+    sunset: response.sys.sunset,
+    icon: '<img src=' + response.weather[0].icon + ' >'
+    };
+
+    $("#image").html('<i class="fa fa-sun-o icon" aria-hidden="true"></i>');
+    // call function to determine background icon
+    weatherIcon(weather.sunrise, weather.sunset, weather.id);
+
+    // append temperature and description
+    $("#temp").html(weather.farenheit);
+    $("#text").html(weather.desc);
+
+    // show convert button now that temp is appended
+    $("#convert").html('<i class="fa fa-random fa-lg" aria-hidden="true"></i>');
+}
+
+function noWeather () {
+    $("#text").html("Something went wrong with the API request :( \n Please try again in a moment");
+}
+
+
+/****************************************************
+ * 
+ * 
+ *              GEOLOCATION RETREVIAL
+ * 
+ * 
+ ****************************************************/
+
+function geoLocation () {
+    // if geolocation is avaliable in the browser
+    if (navigator.geolocation) {
+        // get users current position
+        navigator.geolocation.getCurrentPosition( geoSuccess , geoFail );
+    } else {
+        // if not avail in broswer let user know
+        $("#text").html("Geolocation not avaliable in your browser :("); 
+    }
+        // if JS and JQuery worked successfully let user know we are at work
+        $("#text").html("Trying to get your position...");
+}
+
+// function the succesful geolocation query calls
+function geoSuccess (response) {
+    // isolcate the lat and long w/o decimals to conform to the API requirements
+    let lat = Math.round(response.coords.latitude);
+    let long = Math.round(response.coords.longitude);
+
+    // print lat and lon to DOM for testing
+    $("#text").html(`Loading...`);
+    // pass successful geoLocation to weather function for weather API request
+    getWeather(lat, long);
+}
+
+// function the failed geolocation query calls
+function geoFail () {
+    $("#text").html("Unable to get your position :(");
+}
---
+++ projects/intermediateProjectSite/js/main.js
@@ -0,0 +1,103 @@
+setLanding();
+showContent();
+showInfo();
+
+// adds event listeners to nav-items, adn locates items to be displayed based on nav-items id
+// nav-item ids are classes on elements to be displayed to locate and idexes them
+document.getElementById('nav').addEventListener('click', x => {
+    // ### if i can find parent of current target li > a the li, then i can add accessiblity fo IE ### \\
+
+    // make sure we don't react to click that are not nav children with proper ids
+    if (x.target !== x.currentTarget && x.target.id !== '') {
+        // save the target element's id
+        let commonClass = x.target.id;
+        // use that id to locate all the elements that should be toggled on click
+        let quoteGen = document.getElementsByClassName(commonClass);
+        
+        // use teh array values obtained to access the proper elements based on their location in the HTML
+        // order is [ nav-items, nav-info, content ]
+        showContent( quoteGen[2] );
+        showInfo( quoteGen[1] );
+        setCurrent( quoteGen[0] );
+    }
+    
+}, false);
+
+// set landing
+// if JS loaded up correctly change the message on the landing page to normal
+function setLanding () {
+    document.getElementById("landing_message").textContent = 'My freeCodeCamp Projects';
+}
+
+// showContent
+// create function to show element input as param
+function showContent ( contentElement ) {
+
+    // vars for elements with 'content' class
+    let sections = document.getElementsByClassName('content');
+
+    // let user toggle back to landing content, set it as teh content if the content is already showing
+    if (contentElement === undefined || contentElement.style.display === 'block'){ 
+        contentElement = document.getElementById("landing");
+    }
+
+    // set all elements with 'content' class to none
+    for (let i = 0; i < sections.length; i++) {   
+
+        sections[i].style.display = 'none';
+    }
+
+    // set correct element to show
+    contentElement.style.display = 'block';
+}
+
+// showInfo
+// create function to show element input as param
+function showInfo ( infoElement = {} ) {
+
+    // vars for elements with 'content' class
+    let panels = document.getElementsByClassName('nav-info');
+
+    // if parameter is a valid inforElement and already set to display, set it to dummy object so it wont be set to show after for loop
+    if (infoElement.style && infoElement.style.display === 'block') {
+        infoElement = {};
+    }
+
+    // set all elements with 'content' class to none except the given element
+    for (let i = 0; i < panels.length; i++) {   
+        panels[i].style.display = 'none';
+    }
+
+    // if a valid infoElement with a .style property was passed in set it to display
+    if (infoElement.style) {
+        infoElement.style.display = 'block';   
+    }
+}
+ 
+
+
+
+// setCurrent
+// create function to check for current-nav on items except param input, remove it if it exists on other items and apply it to the element associated with param input if its not already
+function setCurrent ( navLink ) {
+    // vars for elements with 'content' class
+    let links = document.getElementsByClassName('nav-item');
+    
+    // if contains class already set bogus object to keep it from being re added below, allows toggling
+    if (navLink.classList && navLink.classList.contains('current-nav')) {
+        navLink = {};
+    }
+
+    // checking all navLinks with 'nav-item for 'current-nav' and removing it
+    for (let i = 0; i < links.length; i++) {
+
+        if(links[i].classList.contains('current-nav')) { 
+            links[i].classList.remove('current-nav');
+        }
+    }
+
+    // if a valid navlink was passed into the function give it the class
+    if (navLink.classList) {
+        navLink.classList.add('current-nav');  
+    }
+}
